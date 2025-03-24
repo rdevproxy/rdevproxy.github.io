0e7a3e2b0a035288b19d7d30d9f30efa024479c1
rdcgomez <dev.renzgomez@gmail.com>
2025-03-24 16:33:17 +0800

    FIX: header and paragraph

--- src/app/hero.tsx
+++ src/app/hero.tsx
@@ -5,7 +5,7 @@ import { Input, Button, Typography } from "@material-tailwind/react";
 
 function Hero() {
   return (
-    <header className="bg-white p-8">
+    <header className="bg-white mb-20 p-8">
       <div className="container mx-auto grid h-full gap-10 min-h-[60vh] w-full grid-cols-1 items-center lg:grid-cols-2">
         <div className="row-start-2 lg:row-auto">
           <Typography
@@ -13,42 +13,20 @@ function Hero() {
             color="blue-gray"
             className="mb-4 lg:text-5xl !leading-tight text-3xl"
           >
-            Welcome to my Web <br /> Development Portofolio!
+            $whoami <br /> Cybersecurity Professional
           </Typography>
           <Typography
             variant="lead"
             className="mb-4 !text-gray-500 md:pr-16 xl:pr-28"
           >
-            I&apos;m Lily Smith, a passionate web developer based in USA. Here,
-            you&apos;ll get a glimpse of my journey in the world of web
-            development, where creativity meets functionality.
+            I am Renz Gomez, a Penetration Tester, Ethical Hacker and Incident Responder based in the Philippines. Get a glimpse of my journey in cybersecurity where I uncover vulnerabilities, strengthen defenses, and ensure digital resilience for organization.
           </Typography>
-          <div className="grid">
-            <Typography
-              variant="small"
-              className="mb-2 text-gray-900 font-medium"
-            >
-              Your email
-            </Typography>
-            <div className="mb-2 flex w-full flex-col gap-4 md:w-10/12 md:flex-row">
-              {/* @ts-ignore */}
-              <Input color="gray" label="Enter your email" size="lg" />
-              <Button color="gray" className="w-full px-4 md:w-[12rem]">
-                require offer
-              </Button>
-            </div>
-          </div>
-          <Typography variant="small" className="font-normal !text-gray-500">
-            Read my{" "}
-            <a href="#" className="font-medium underline transition-colors">
-              Terms and Conditions
-            </a>
-          </Typography>
+        
         </div>
         <Image
           width={1024}
           height={1024}
-          alt="team work"
+          alt="hero-image"
           src="/image/image-7.svg"
           className="h-[36rem] w-full rounded-xl object-cover"
         />
